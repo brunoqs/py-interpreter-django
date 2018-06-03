d3e68c11ce0882293cf3928186bf0f17ae9123fe
brunoqs <brun_o13@hotmail.com>
2018-06-03 00:34:48 -0300

    now its working

--- core/views.py
+++ core/views.py
@@ -4,14 +4,23 @@ import subprocess
 # Create your views here.
 
 def index(request):
-	output = ''
+	#output = ''
 	if request.method == 'POST':
 		code = request.POST.get('code')
 		f = open('file.py', 'w+')
 		f.write(str(code))
-		output = subprocess.check_output("python /home/unclear/Projetos/pyweb/file.py", stderr=subprocess.STDOUT, shell=True)
+		#output = subprocess.check_output(["python /home/unclear/Projetos/pyweb/file.py"], shell=True)
+		#context = {'code': output}
+		#print (output.decode("utf-8"))
+		return render(request, "core/index.html", {})
+	else:
+		return render(request, "core/index.html", {})
+
+def run(request):
+	if request.method == 'POST':
+		output = subprocess.check_output(["python /home/unclear/Projetos/pyweb/file.py"], shell=True)
+		output = str(output)
 		context = {'code': output}
-		print (output.decode("utf-8"))
 		return render(request, "core/index.html", context)
 	else:
 		return render(request, "core/index.html", {})
